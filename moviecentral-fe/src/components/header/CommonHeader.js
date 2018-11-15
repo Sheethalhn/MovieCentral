67db66145c0cae31a64ecfd9f2a2a19b32cdc4e9
shreyashah7 <shreya.shah@sjsu.edu>
2018-11-14 19:11:42 -0800

    Logout functionality added

--- moviecentral-fe/src/components/header/CommonHeader.js
+++ moviecentral-fe/src/components/header/CommonHeader.js
@@ -10,12 +10,20 @@ import { LinkContainer } from 'react-router-bootstrap';
 import moviecentral from './moviecentral.png';
 import './header.css';
 import { loginUser } from "../../actions";
+import * as API from '../../api/API';
 
 class CommonHeader extends Component {
 
     constructor(props) {
         super(props);
         console.log("user props :", this.props.user);
+    }
+
+    logout() {
+        API.logout().then(() => {
+            window.location = "/"
+            this.props.loginUser(null);
+        })
     }
 
     render() {
@@ -34,7 +42,7 @@ class CommonHeader extends Component {
                             <li className="navigation-tab">Recently watched</li>
                             <li className="navigation-tab nav-drop">
                                 <a>Welcome, <span className="user-name-color">{this.props.user !== undefined && this.props.user !== null ? this.props.user.firstName + " " + this.props.user.lastName : ''}</span></a>
-                                <a>Logout</a>
+                                <a onClick={this.logout}>Logout</a>
                             </li>
                         </ul>
                     </div>
@@ -50,7 +58,7 @@ function mapStateToProps(state) {
     }
 }
 function matchDispatchToProps(dispatch) {
-    return bindActionCreators({loginUser:loginUser}, dispatch)
+    return bindActionCreators({ loginUser: loginUser }, dispatch)
 }
 
 export default connect(mapStateToProps, matchDispatchToProps)(CommonHeader);
