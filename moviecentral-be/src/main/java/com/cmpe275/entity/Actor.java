f492e6321031196ca68a85bbd6ef41b57c34f17d
Tarun Arora <tarunarora0807@gmail.com>
2018-11-30 20:01:47 -0800

    Actor Backend CRUD added

--- moviecentral-be/src/main/java/com/cmpe275/entity/Actor.java
+++ moviecentral-be/src/main/java/com/cmpe275/entity/Actor.java
@@ -5,15 +5,21 @@
  */
 package com.cmpe275.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.springframework.lang.Nullable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  *
@@ -27,13 +33,20 @@ public class Actor {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "actor_id")
     private Long actorId;
+    @NotNull
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    @JsonBackReference
+    private Actor actor;
 
     @Override
     public String toString() {
         return "{" +
             " actorId='" + getActorId() + "'" +
             ", name='" + getName() + "'" +
+            ", actor='" + getActor() + "'" +
             "}";
     }
 
@@ -52,5 +65,13 @@ public class Actor {
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    public Actor getActor() {
+        return this.actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
 }
