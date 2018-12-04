c750222f23fe6272d052d0a8726026db21f71f8f
Tarun Arora <tarunarora0807@gmail.com>
2018-12-03 20:00:11 -0800

    dashboard import fixed

--- moviecentral-fe/src/components/admin/AddEditMovie/editMovie.js
+++ moviecentral-fe/src/components/admin/AddEditMovie/editMovie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CommonHeader from '../../header/CommonHeader';
-import Sidebar from "../Dashboard/Sidebar";
+import Sidebar from "../dashboard/Sidebar";
 import FullMovieForm from "./FullMovieForm";
 import { getOneMovie } from "../../../api/API";
 
