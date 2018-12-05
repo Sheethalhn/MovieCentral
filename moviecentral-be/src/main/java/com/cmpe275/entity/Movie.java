79bf63d2446d5365adfe04019c031114f7edaa3d
rachitchokshi <rachitchokshi@gmail.com>
2018-12-04 18:47:52 -0800

    -added fields for average ratings and total reviews for each movie

--- moviecentral-be/src/main/java/com/cmpe275/entity/Movie.java
+++ moviecentral-be/src/main/java/com/cmpe275/entity/Movie.java
@@ -20,9 +20,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Formula;
 import org.springframework.data.annotation.Transient;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import org.springframework.data.jpa.repository.Query;
 
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -60,11 +62,24 @@ public class Movie {
     private Double price;
     private Integer stars;
 
-   
+    @Formula("(select avg(r.rating) FROM review r where r.movie_id = movie_id)")
+    private Double avgratings;
+
+    @Formula("(select COUNT(r.review_id) FROM review r where r.movie_id = movie_id)")
+    private Double totalreviews;
+
+    public Double getAvgratings() {
+        return avgratings;
+    }
+
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name="actor_id")
     private List<Actor> actors = new ArrayList<>();
-    
+
+    public Double getTotalreviews() {
+        return totalreviews;
+    }
+
     @OneToMany(mappedBy = "movieObj", fetch = FetchType.LAZY)
     @JsonManagedReference(value = "movie-reference")
     private List<PlaybackHistory> moviePlaybackHistory = new ArrayList<>();
